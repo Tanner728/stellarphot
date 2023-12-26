a726c7acd310c631a42f3a396919306ec941f6d4
Tanner Weyer <tweyer28@hotmail.com>
2023-12-26 10:09:29 -0600

    Add docum to validate functions

--- stellarphot/settings/models.py
+++ stellarphot/settings/models.py
@@ -164,6 +164,9 @@ class Exoplanet(BaseModel):
 
     @classmethod
     def validate_period(cls, values):
+        """
+        Checks that the period has physical units of time and raises an error if that is not true. 
+        """
         if u.get_physical_type(values["period"]) != "time":
             raise ValueError(
                 f"Period does not have time units,currently has {values['period'].unit} units."
@@ -171,6 +174,9 @@ class Exoplanet(BaseModel):
 
     @classmethod
     def validate_duration(cls, values):
+        """
+        Checks that the duration has physical units of time and raises an error if that is not true. 
+        """
         if u.get_physical_type(values["duration"]) != "time":
             raise ValueError(
                 f"Period does not have time units,currently has {values['duration'].unit} units."
